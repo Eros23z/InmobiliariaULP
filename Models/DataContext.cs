140936484048e4c4d097eae22f9581fb3109efc2
Eros <energycs23@live.com>
2026-08-26 23:48:35 -0300

    Creacion de modelos de inquilino, propietario y datacontext

---
+++ Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InmobiliariaULP.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Propietario> Propietarios { get; set; }
+        public DbSet<Inquilino> Inquilinos { get; set; }
+    }
+}
